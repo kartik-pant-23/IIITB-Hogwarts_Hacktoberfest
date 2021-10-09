1e223a68a6736dbfbd52de18bffe323dfa5c931b
shikhar13012001 <75368010+shikhar13012001@users.noreply.github.com>
2021-10-09 22:51:56 +0530

    added Proper response to invalid authentication attempt

--- backend/api/middlewares/auth.js
+++ backend/api/middlewares/auth.js
@@ -1,27 +1,26 @@
 // TODO: Perform JWT authentication here
 const jwt = require("jsonwebtoken");
-const User= require("../models/user");
+const User = require("../models/user");
 
+const requireAuth = (req, res, next) => {
+  //require the token from client headers
+  const token = req.headers.authorization.split(" ")[1];
+  if (token) {
+    User.findByToken(token, (err, user) => {
+      if (err) {
+        next(err);
+      } else {
+        if (user) {
+          req.user = user;
+          next();
+        } else {
+          res.status(401).json({ message: "Authentication failed!" });
+        }
+      }
+    });
+  } else {
+    res.status(401).json({ message: "Authentication failed!" });
 
-const requireAuth=(req,res,next)=>{
-    //require the token from client headers
-    const token= req.headers.authorization.split(' ')[1];
-    if(token)
-    {
-    User.findByToken(token,(err, user)=>{
-        if(err)
-        {
-            next(err);
-        }
-        else
-        {    req.user= user;
-             next();
-        }
-    })
-   }
-   else
-   {
-       throw Error("User must be logged in");
-   }
-}
-module.exports=requireAuth;
+  }
+};
+module.exports = requireAuth;
